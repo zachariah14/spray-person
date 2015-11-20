aaa52ae4e99a25a56d5160fe820e012b97921a5d
andriypa <andriy.panov@playtech.com>
2015-11-20 17:09:53 +0200

    spray - removing obsolete class and updating existed one

--- src/main/scala/com/example/actors/routes/PersonRoute.scala
+++ src/main/scala/com/example/actors/routes/PersonRoute.scala
@@ -2,7 +2,7 @@ package com.example.actors.routes
 
 import com.example.model.Person
 import com.example.model.PersonJsonProtocol._
-import com.example.services.{PersonServiceImpl, PersonService}
+import com.example.services.PersonService
 import akka.actor.Props
 import spray.http.StatusCodes
 import spray.httpx.SprayJsonSupport._
@@ -12,27 +12,27 @@ import akka.actor.Actor
 import org.slf4j.LoggerFactory
 
 /**
-* Factory method for Props configuration files for actors
-*/
+  * Factory method for Props configuration files for actors
+  */
 object PersonRoute {
   def props: Props = Props(new PersonRoute())
 }
 
 /**
- * Actor that handles requests that begin with "person"
- */
+  * Actor that handles requests that begin with "person"
+  */
 class PersonRoute() extends Actor with PersonRouteTrait {
   def actorRefFactory = context
   def receive = runRoute(personRoute)
 }
 
 /**
- * Separate routing logic in an HttpService trait so that the
- * routing logic can be tested outside of an actor system in specs/mockito tests
- */
+  * Separate routing logic in an HttpService trait so that the
+  * routing logic can be tested outside of an actor system in specs/mockito tests
+  */
 trait PersonRouteTrait extends HttpService with SprayJsonSupport{
 
-  private val personService = new PersonServiceImpl
+  private val personService = PersonService
   val log = LoggerFactory.getLogger(classOf[PersonRouteTrait])
 
   val personRoute = {
@@ -47,34 +47,34 @@ trait PersonRouteTrait extends HttpService with SprayJsonSupport{
           }
         }
       } ~
-      path(LongNumber) { personId =>
-        log.debug(s"Hitting Get Person by Id:${personId}")
-        val person = personService.getPersonById(personId)
-        complete(person)
-      }
-    } ~
-    (post & pathEnd) {
-      entity(as[Person]) { person =>
-        log.debug("posting to create a person")
-        val newPerson = personService.addPerson(person)
-        complete(StatusCodes.Created, newPerson)
-      }
-    } ~
-    (put & path(LongNumber) & pathEnd) { personId =>
-      entity(as[Person]) { person =>
-        log.debug(s"updating a person with the id: ${personId}")
-        val updatedPerson = personService.updatePerson(person.copy(id = Some(personId)))
-        updatedPerson match {
-          case true => complete(StatusCodes.NoContent)
-          case false => complete(StatusCodes.NotFound)
+        path(LongNumber) { personId =>
+          log.debug(s"Hitting Get Person by Id:${personId}")
+          val person = personService.getPersonById(personId)
+          complete(person)
         }
-      }
     } ~
-    (delete & path(LongNumber) & pathEnd) { personId =>
-       log.debug(s"deleting a person with the id: ${personId}")
-      personService.deletePerson(personId)
-      complete(StatusCodes.NoContent)
-    }
+      (post & pathEnd) {
+        entity(as[Person]) { person =>
+          log.debug("posting to create a person")
+          val newPerson = personService.addPerson(person)
+          complete(StatusCodes.Created, newPerson)
+        }
+      } ~
+      (put & path(LongNumber) & pathEnd) { personId =>
+        entity(as[Person]) { person =>
+          log.debug(s"updating a person with the id: ${personId}")
+          val updatedPerson = personService.updatePerson(person.copy(id = Some(personId)))
+          updatedPerson match {
+            case true => complete(StatusCodes.NoContent)
+            case false => complete(StatusCodes.NotFound)
+          }
+        }
+      } ~
+      (delete & path(LongNumber) & pathEnd) { personId =>
+        log.debug(s"deleting a person with the id: ${personId}")
+        personService.deletePerson(personId)
+        complete(StatusCodes.NoContent)
+      }
   }
 
 }
